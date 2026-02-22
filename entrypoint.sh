a4706d23ee3422b6ed089b7ef11ac686a384a31c
mrbeenopro <mrbeenopro@gmail.com>
2026-02-22 11:47:04 +0700

    Adjust sleep time and startup command in entrypoint.sh
    Updated sleep duration and modified startup command for QEMU and KasmVNC.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,19 +3,18 @@
 export HOME=/home/container
 mkdir -p $HOME/.vnc $HOME/tmp
 chmod 700 $HOME/.vnc
-
 export TMPDIR=$HOME/tmp
 
 VNC_PORT=5901
 WS_PORT=${SERVER_PORT:-8443}
 
-echo "[+] User: $(id -u)"
-echo "[+] HOME: $HOME"
-
+echo "[+] Starting QEMU..."
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 eval ${MODIFIED_STARTUP} &
 
-sleep 2
+sleep 3
+
+echo "[+] Starting KasmVNC WebSocket proxy..."
 
 kasmvncserver \
   --vnc-host localhost \
@@ -23,4 +22,6 @@ kasmvncserver \
   --listen 0.0.0.0 \
   --port ${WS_PORT} \
   --no-desktop \
-  --cert none
+  --no-auth \
+  --cert none \
+  --non-interactive
